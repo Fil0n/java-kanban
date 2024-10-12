e430905b96a748e9a8e592a78f3e6cab349a551e
Alexandr <lexfilonenko@yandex.ru>
2024-10-12 10:48:25 +0300

    HistoryManager в final в InMemoryTaskManager

--- src/com/yandex/app/service/InMemoryTaskManager.java
+++ src/com/yandex/app/service/InMemoryTaskManager.java
@@ -10,7 +10,7 @@ public class InMemoryTaskManager implements TaskManager {
     private final HashMap<Integer, Task> tasks = new HashMap<>();
     private final HashMap<Integer, Epic> epics = new HashMap<>();
     private final HashMap<Integer, Subtask> subtasks = new HashMap<>();
-    private HistoryManager historyManager = Managers.getDefaultHistory();
+    private final HistoryManager historyManager = Managers.getDefaultHistory();
     private int counter = 0;
 
     @Override
