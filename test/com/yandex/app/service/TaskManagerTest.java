1b244d844fac1434a3ee65f953485174bef36b9e
Alexandr <lexfilonenko@yandex.ru>
2024-11-16 17:45:52 +0300

    Удалил путые строки

--- test/com/yandex/app/service/TaskManagerTest.java
+++ test/com/yandex/app/service/TaskManagerTest.java
@@ -79,6 +79,4 @@ public class TaskManagerTest {
 
         assertEquals(Status.DONE, savedEpic.getStatus(), "Неверный статус епика " + savedEpic.getStatus() + " ожидается " + Status.DONE);
     }
-
-
 }
