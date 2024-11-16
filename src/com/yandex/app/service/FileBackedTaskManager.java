343c89a33dc2dfea3d9a91de48f952f397eca8df
Alexandr <lexfilonenko@yandex.ru>
2024-11-16 14:10:17 +0300

    Добавлен метод load для загрузки данных из файла,  и  проведен небольшой рефакторинг

--- src/com/yandex/app/service/FileBackedTaskManager.java
+++ src/com/yandex/app/service/FileBackedTaskManager.java
@@ -3,35 +3,42 @@ package com.yandex.app.service;
 import com.yandex.app.model.Epic;
 import com.yandex.app.model.Subtask;
 import com.yandex.app.model.Task;
+import com.yandex.app.model.TaskType;
 import com.yandex.app.util.ManagerSaveException;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
 
+    final private String DEFAULT_FILE_NAME = "Tasks.csv";
+
     public void save() {
-        try (FileWriter fileWriter = new FileWriter("Tasks.csv")) {
+        save(DEFAULT_FILE_NAME);
+    }
+
+    public void save(String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
             List<Task> tasks = getTasks();
             if (tasks.isEmpty()) {
                 for (Task task : tasks) {
-                    fileWriter.append(task.toString());
+                    writer.append(task.toString());
                 }
             }
 
             List<Epic> epics = getEpics();
             if (epics.isEmpty()) {
                 for (Epic epic : epics) {
-                    fileWriter.append(epic.toString());
+                    writer.append(epic.toString());
                 }
             }
 
             List<Subtask> subtasks = getSubtasks();
             if (!subtasks.isEmpty()) {
                 for (Subtask subtask : subtasks) {
-                    fileWriter.append(subtask.toString());
+                    writer.append(subtask.toString());
                 }
             }
         } catch (ManagerSaveException e) {
@@ -41,8 +48,36 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         }
     }
 
+//    public void load(){
+//        load(DEFAULT_FILE_NAME);
+//    }
+
+    public void load(File file) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
+            reader.readLine();
+            while (true) {
+                String line = reader.readLine();
+                if (line.isEmpty()) {
+                    break;
+                }
+
+                final TaskType type = Task.typeFromString(line);
+
+                switch (type) {
+                    case TASK -> addTask(Task.fromString(line));
+                    case EPIC -> addEpic(Epic.fromString(line));
+                    default -> addSubtask(Subtask.fromString(line));
+                }
+            }
+        } catch (NullPointerException e) {
+            return;
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка в файле: " + file.getAbsolutePath(), e);
+        }
+    }
+
     @Override
-    public Integer addSubtask(Subtask subtask) {
+    public Integer addSubtask(Task subtask) {
         int id = super.addSubtask(subtask);
         save();
         return id;
@@ -54,8 +89,8 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         save();
         return id;
     }
-
-    public Integer addEpic(Epic epic) {
+    @Override
+    public Integer addEpic(Task epic) {
         int id = super.addEpic(epic);
         save();
         return id;
