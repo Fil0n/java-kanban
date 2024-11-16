8aded1eecff035cab8986d687e2f845541a19372
Alexandr <lexfilonenko@yandex.ru>
2024-11-16 17:48:25 +0300

    Добавлен тест на удаление сабтасков

--- test/com/yandex/app/service/FileBackedTaskManagerTest.java
+++ test/com/yandex/app/service/FileBackedTaskManagerTest.java
@@ -76,4 +76,25 @@ public class FileBackedTaskManagerTest {
         assertEquals(subtask1.getName(), fTaskMenager.getSubtaskById(subtaskId).getName(), "Сабтаски с идентификатором 2 не соответствуют");
         assertEquals(subtask2.getName(), fTaskMenager.getSubtaskById(subtask2Id).getName(), "Сабтаски с идентификатором 3 не соответствуют");
     }
+
+    @Test
+    void removeSubtaskOnFile() {
+        Epic epic1 = new Epic("Epic 1", "Описание 1");
+        int epic1Id = taskManager.addEpic(epic1);
+
+        Subtask subtask1 = new Subtask("Subtask 1", "Описание 1", epic1Id);
+        int subtaskId = taskManager.addSubtask(subtask1);
+
+        Subtask subtask2 = new Subtask("Subtask 2", "Описание 1", epic1Id);
+        int subtask2Id = taskManager.addSubtask(subtask2);
+
+        taskManager.removeSubtaskById(subtask2Id);
+
+        FileBackedTaskManager fTaskMenager = new FileBackedTaskManager();
+
+        assertEquals(1, fTaskMenager.epics.size(), "Неверное количество эпиков.");
+        assertEquals(1, fTaskMenager.getEpicById(epic1Id).getSubtasksIds().size(), "Неверное количество сабтасков в епике");
+        assertEquals(1, fTaskMenager.subtasks.size(), "Неверное количество сабтасков.");
+        assertEquals(subtask1.getName(), fTaskMenager.getSubtaskById(subtaskId).getName(), "Сабтаски с идентификатором 2 не соответствуют");
+    }
 }
