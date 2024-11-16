96cf203b3f1f3b6a60e771f6560fad39953cf8d3
Alexandr <lexfilonenko@yandex.ru>
2024-11-16 12:40:16 +0300

    Доработан метод сохрания задач в файл

--- src/com/yandex/app/service/FileBackedTaskManager.java
+++ src/com/yandex/app/service/FileBackedTaskManager.java
@@ -3,11 +3,42 @@ package com.yandex.app.service;
 import com.yandex.app.model.Epic;
 import com.yandex.app.model.Subtask;
 import com.yandex.app.model.Task;
+import com.yandex.app.util.ManagerSaveException;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
 
     public void save() {
+        try (FileWriter fileWriter = new FileWriter("Tasks.csv")) {
+            List<Task> tasks = getTasks();
+            if (tasks.isEmpty()) {
+                for (Task task : tasks) {
+                    fileWriter.append(task.toString());
+                }
+            }
 
+            List<Epic> epics = getEpics();
+            if (epics.isEmpty()) {
+                for (Epic epic : epics) {
+                    fileWriter.append(epic.toString());
+                }
+            }
+
+            List<Subtask> subtasks = getSubtasks();
+            if (!subtasks.isEmpty()) {
+                for (Subtask subtask : subtasks) {
+                    fileWriter.append(subtask.toString());
+                }
+            }
+        } catch (ManagerSaveException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
