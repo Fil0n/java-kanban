1864c286a9d5c1bcfac42dc1ff4fae8c8b3439a8
Alexandr <lexfilonenko@yandex.ru>
2024-11-16 11:32:02 +0300

    Добавлен метод сохранения и доработаны методы добавления задач

--- src/com/yandex/app/service/FileBackedTaskManager.java
+++ src/com/yandex/app/service/FileBackedTaskManager.java
@@ -1,4 +1,32 @@
 package com.yandex.app.service;
 
+import com.yandex.app.model.Epic;
+import com.yandex.app.model.Subtask;
+import com.yandex.app.model.Task;
+
 public class FileBackedTaskManager extends InMemoryTaskManager {
+
+    public void save() {
+
+    }
+
+    @Override
+    public Integer addSubtask(Subtask subtask) {
+        int id = super.addSubtask(subtask);
+        save();
+        return id;
+    }
+
+    @Override
+    public Integer addTask(Task task) {
+        int id = super.addTask(task);
+        save();
+        return id;
+    }
+
+    public Integer addEpic(Epic epic) {
+        int id = super.addEpic(epic);
+        save();
+        return id;
+    }
 }
