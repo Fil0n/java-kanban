3b06f325cc0f7c4ea0e063006749f92ecf46cb47
Alexandr <lexfilonenko@yandex.ru>
2024-11-16 12:21:16 +0300

    Добавлено исключение ManagerSaveException

---
+++ src/com/yandex/app/util/ManagerSaveException.java
@@ -0,0 +1,9 @@
+package com.yandex.app.util;
+
+import java.io.IOException;
+
+public class ManagerSaveException extends IOException {
+    public ManagerSaveException(String message) {
+        super(message);
+    }
+}
