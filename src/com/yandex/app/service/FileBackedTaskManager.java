39a5961702277b0b4ef4dbc92dc5f639fc5c4fce
Alexandr <lexfilonenko@yandex.ru>
2024-11-18 10:01:45 +0300

    Убрано использование импорта через *

--- src/com/yandex/app/service/FileBackedTaskManager.java
+++ src/com/yandex/app/service/FileBackedTaskManager.java
@@ -1,6 +1,9 @@
 package com.yandex.app.service;
 
-import com.yandex.app.model.*;
+import com.yandex.app.model.Epic;
+import com.yandex.app.model.Task;
+import com.yandex.app.model.Subtask;
+import com.yandex.app.model.TaskType;
 import com.yandex.app.util.ManagerSaveException;
 
 import java.io.*;
