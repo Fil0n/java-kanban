c7106d10884e18d0812882c537ce9e1d3584b03d
Alexandr <lexfilonenko@yandex.ru>
2024-11-30 15:23:40 +0300

    Обновлены кострукторы эпиков

--- src/com/yandex/app/model/Epic.java
+++ src/com/yandex/app/model/Epic.java
@@ -1,5 +1,7 @@
 package com.yandex.app.model;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +11,36 @@ public class Epic extends Task {
 
     public Epic(int id, String name, String description, Status status) {
         super(id, name, description, status);
+
+        this.subtaskIds.clear();
+
+        for (Integer subtaskId : subtaskIds) {
+            this.subtaskIds.add(subtaskId);
+        }
+    }
+
+    public Epic(int id, String name, String description, Status status, int duration, LocalDateTime startTime) {
+        super(id, name, description, status, duration, startTime);
+
+        this.subtaskIds.clear();
+
+        for (Integer subtaskId : subtaskIds) {
+            this.subtaskIds.add(subtaskId);
+        }
+    }
+
+    public Epic(int id, String name, String description, Status status, LocalDateTime startTime) {
+        super(id, name, description, status, startTime);
+
+        this.subtaskIds.clear();
+
+        for (Integer subtaskId : subtaskIds) {
+            this.subtaskIds.add(subtaskId);
+        }
+    }
+
+    public Epic(int id, String name, String description, Status status, int duration) {
+        super(id, name, description, status, duration);
 
         this.subtaskIds.clear();
 
@@ -48,13 +80,13 @@ public class Epic extends Task {
 
     public static Task fromString(String taskString) {
         String[] data = taskString.split(",");
-        final int parsingParamsCount = 5;
+        final int parsingParamsCount = 7;
 
         if (data.length != parsingParamsCount) {
             return null;
         }
 
-        return new Epic(Integer.parseInt(data[1]), data[2], data[3], Status.valueOf(data[4]));
+        return new Epic(Integer.parseInt(data[1]), data[2], data[3], Status.valueOf(data[4]), data[5].isBlank() ? null : Integer.parseInt(data[5]), data[6].isBlank() ? null : LocalDateTime.parse(data[6]));
 
     }
 
