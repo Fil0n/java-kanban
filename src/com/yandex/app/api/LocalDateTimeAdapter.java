64ef092d0efc15845122ab38b8c4535d77d8c9d8
Alexandr <lexfilonenko@yandex.ru>
2024-12-15 02:37:56 +0300

    Кодстайл

--- src/com/yandex/app/api/LocalDateTimeAdapter.java
+++ src/com/yandex/app/api/LocalDateTimeAdapter.java
@@ -12,12 +12,12 @@ public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
     private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
 
     @Override
-    public void write(final JsonWriter jsonWriter, final LocalDateTime LocalDateTime) throws IOException {
-        if (LocalDateTime == null) {
+    public void write(final JsonWriter jsonWriter, final LocalDateTime localDateTime) throws IOException {
+        if (localDateTime == null) {
             jsonWriter.nullValue();
             return;
         }
-        jsonWriter.value(LocalDateTime.format(timeFormatter));
+        jsonWriter.value(localDateTime.format(timeFormatter));
     }
 
     @Override
