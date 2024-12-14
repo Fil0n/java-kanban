d7d31f8347505116f595dd0687ccc018665cca88
Alexandr <lexfilonenko@yandex.ru>
2024-12-14 23:31:59 +0300

    Рефакторинг в Базовом хендлере

--- src/com/yandex/app/api/BaseHandler.java
+++ src/com/yandex/app/api/BaseHandler.java
@@ -1,81 +1,51 @@
 package com.yandex.app.api;
 
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
 import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class BaseHandler {
     public static void sendText(HttpExchange exchange, String text) throws IOException {
-        if(text.equals("null")) {
+        if (text.equals("null")) {
             sendNotFound(exchange);
-            return;
+        } else {
+            sendResponse(exchange, 200, text);
         }
-
-        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        exchange.sendResponseHeaders(200, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
     }
 
     public static void sendNotFound(HttpExchange exchange) throws IOException {
-        byte[] resp = ("{\"message\":\"Not Found\"}").getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        exchange.sendResponseHeaders(404, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+        sendResponse(exchange, 404, "{\"message\":\"Not Found\"}");
     }
 
     public static void sendDeleted(HttpExchange exchange) throws IOException {
-        byte[] resp = ("{\"message\":\"Deleted\"}").getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        exchange.sendResponseHeaders(200, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+        sendResponse(exchange, 200, "{\"message\":\"Deleted\"}");
     }
 
     public static void sendBadRequest(HttpExchange exchange) throws IOException {
-        byte[] resp = ("{\"message\":\"Bad Request\"}").getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        exchange.sendResponseHeaders(400, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+        sendResponse(exchange, 400, "{\"message\":\"Bad Request\"}");
     }
 
     public static void sendServerError(HttpExchange exchange) throws IOException {
-        byte[] resp = ("{\"message\":\"Server Error\"}").getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        exchange.sendResponseHeaders(500, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+        sendResponse(exchange, 500, "{\"message\":\"Server Error\"}");
     }
 
     public static void sendCreated(HttpExchange exchange) throws IOException {
-        byte[] resp = ("{\"message\":\"Created\"}").getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        exchange.sendResponseHeaders(201, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+        sendResponse(exchange, 201, "{\"message\":\"Created\"}");
     }
 
     public static void sendUpdated(HttpExchange exchange) throws IOException {
-        byte[] resp = ("{\"message\":\"Updated\"}").getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        exchange.sendResponseHeaders(202, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+        sendResponse(exchange, 202, "{\"message\":\"Updated\"}");
     }
 
     public static void sendNonAcceptable(HttpExchange exchange) throws IOException {
-        byte[] resp = ("{\"message\":\"Not Acceptable\"}").getBytes(StandardCharsets.UTF_8);
+        sendResponse(exchange, 406, "{\"message\":\"Not Acceptable\"}");
+    }
+
+    private static void sendResponse(HttpExchange exchange, int status, String text) throws IOException{
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
         exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        exchange.sendResponseHeaders(406, resp.length);
+        exchange.sendResponseHeaders(status, resp.length);
         exchange.getResponseBody().write(resp);
         exchange.close();
     }
